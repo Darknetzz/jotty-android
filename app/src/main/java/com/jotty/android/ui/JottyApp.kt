89a042f8871381423423b6113442a4a015987db3
Kristian Røste <github@roste.org>
2026-02-06 09:52:10 +0100

    Refactor JottyApp.kt to include additional Compose animations and import MainScreen and SetupScreen components

--- app/src/main/java/com/jotty/android/ui/JottyApp.kt
+++ app/src/main/java/com/jotty/android/ui/JottyApp.kt
@@ -1,11 +1,16 @@
 package com.jotty.android.ui
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.jotty.android.data.preferences.SettingsRepository
+import com.jotty.android.ui.main.MainScreen
+import com.jotty.android.ui.setup.SetupScreen
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 

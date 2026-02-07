a8f3d7d5a29df8fe61aac1c47d0dd5d4cab50849
Kristian Røste <github@roste.org>
2026-02-07 16:14:50 +0100

    Add lifecycle-process dependency to build.gradle.kts
    - Included androidx.lifecycle:lifecycle-process:2.8.7 to enhance lifecycle management capabilities in the application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
 
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-process:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.11.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
